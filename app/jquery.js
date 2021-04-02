abea5635bd8fa2dce989d89f51bd974ff41ed4b0
= <=>
2021-04-02 20:13:02 +0530

    Login Backend Intgrated with Front End and Code Editor

---
+++ app/jquery.js
@@ -0,0 +1,23 @@
+$(".btn1").click(function(){
+    $("#buttons").css("display","none");
+    $(".login-form").css("display","block");
+    $(".reg-form").css("display","none");
+})
+
+$(".btn2").click(function(){
+    $("#buttons").css("display","none");
+    $(".reg-form").css("display","block");
+    $(".login-form").css("display","none");
+})
+
+$(".btnr2").click(function(){
+    $("#buttons").css("display","none");
+    $(".reg-form").css("display","none");
+    $(".login-form").css("display","block");
+})
+
+$(".btnl2").click(function(){
+    $("#buttons").css("display","none");
+    $(".login-form").css("display","none");
+    $(".reg-form").css("display","block");
+})
